265df4739e1793317a552c05cd04d50f8e609664
Deepakkv <deepakkv@in-deepakkv.local>
2017-05-04 20:16:15 +0530

    Google maps integrated

--- Go Home/AppDelegate.swift
+++ Go Home/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyAXVDvW4SL2EJ7OIvIW_WF_hfNRBPQuF9I")
+        GMSPlacesClient.provideAPIKey("AIzaSyAXVDvW4SL2EJ7OIvIW_WF_hfNRBPQuF9I")
         return true
     }
 
